1c8df636ac85e98b814f2eb9d59d9ea94f3ce458
Gijs Molenaar <gijs@pythonic.nl>
2016-03-14 12:31:34 +0200

    fix tests

--- test/test_validate.py
+++ test/test_validate.py
@@ -1,24 +1,35 @@
 import os
 import unittest
 import json
+import yaml
 
 import kliko.validate
 
 here = os.path.dirname(os.path.realpath(__file__))
+kliko_file = os.path.join(here, "../examples/kliko.yml")
+parameters_file = os.path.join(here, "../examples/parameters.json")
 
 
 class TestExample(unittest.TestCase):
-    def setUp(self):
-        self.kliko_example = open(os.path.join(here, "../examples/kliko.yml"), 'r').read()
-        self.parameters_example = open(os.path.join(here, "../examples/parameters.json"), 'r').read()
+    @classmethod
+    def setUpClass(cls):
+        with open(kliko_file, 'r') as f:
+            cls.kliko_data = yaml.safe_load(f)
+
+        with open(parameters_file, 'r') as f:
+            cls.parameters_data = json.load(f)
 
     def test_validate_kliko(self):
-        kliko.validate.validate_kliko(self.kliko_example)
+        kliko.validate.validate_kliko(self.kliko_data)
+
+    def test_convert_to_parameters_schema(self):
+        kliko.validate.convert_to_parameters_schema(self.kliko_data)
 
     def test_validate_parameters(self):
-        validator = kliko.validate.validate_kliko(self.kliko_example)
-        validated = json.loads(self.parameters_example)
-        kliko.validate.validate_parameters(validated, validator)
+        kliko.validate.validate_parameters(self.parameters_data, self.kliko_data)
+
+    def test_validate(self):
+        kliko.validate.validate(kliko_file, parameters_file)
 
 
 
