9f6bdda3b8616da0efc3cd2b125024d5a62c89ca
Gijs Molenaar <gijs@pythonic.nl>
2016-03-14 14:03:38 +0200

    cleanup setup procedure

--- setup.py
+++ setup.py
@@ -2,18 +2,30 @@ from setuptools import setup, find_packages
 
 from kliko.version import __version__
 
+install_requires = [
+    'pykwalify',
+    'PyYAML',
+    'future',
+    'docopt'
+]
+
+extras_require = {
+    'django': ['django', 'django-form-utils'],
+    'docker': ['docker-py'],
+}
+
+
 setup(
     name="kliko",
     version=__version__,
     packages=find_packages(),
     scripts=['bin/kliko-validate.py'],
-    install_requires=['pykwalify', 'PyYAML', 'docker-py', 'future', 'docopt'],
-
+    install_requires=install_requires,
+    extras_require=extras_require,
     package_data={
         '': ['*.txt', '*.rst'],
         'kliko': ['schemas/*.yml'],
     },
-
     author="Gijs Molenaar",
     author_email="gijs@pythonic.nl",
     description="Scientific Compute Container Spec Parser",
