62322ca9ea38792b1ebe6a41b0339064eec84c5a
Gijs Molenaar <gijs@pythonic.nl>
2016-04-08 15:26:26 +0200

    don't import, since we may not have docker installed

--- kliko/__init__.py
+++ kliko/__init__.py
@@ -1,7 +1,5 @@
 import pkg_resources
 
-from kliko.cli import generate_kliko_cli_parser
-from kliko.kliko_docker import extract_params
 
 try:
     __version__ = pkg_resources.require("radiopadre")[0].version
