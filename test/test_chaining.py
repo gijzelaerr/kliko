3a54d8848d34e9db1695080a62aea33e7806ec18
Gijs Molenaar <gijs@pythonic.nl>
2016-12-07 18:08:16 +0200

    so hashing is different for every python

--- test/test_chaining.py
+++ test/test_chaining.py
@@ -7,7 +7,6 @@ class TestChaining(unittest.TestCase):
 
         x1 = {'a': 'b', 'b': 'a'}
         x2 = {'b': 'a', 'a': 'b'}
-        xhash = '38dd8d5719784dcd2a7ac82f418b93fc4af827032313dea5e5d0cab742a958fb'
 
-        self.assertEqual(kliko.chaining._dict2sha256(x1), xhash)
-        self.assertEqual(kliko.chaining._dict2sha256(x2), xhash)
+        self.assertEqual(kliko.chaining._dict2sha256(x1), kliko.chaining._dict2sha256(x2))
+
